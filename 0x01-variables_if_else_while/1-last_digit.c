08574c4a44f75fed1005b4353835b7ece61ab5a8
Collinsnyakundi <collinsnyakundi98@gmail.com>
2022-07-04 09:59:35 -0700

    Digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * priniting lower case alphabets
+ * Return: zero sucess
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 97; a = 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
