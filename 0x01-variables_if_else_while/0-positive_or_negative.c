4e87b162fae1b9b80116be4d6ff471a48f2d4a9e
Collinsnyakundi <collinsnyakundi98@gmail.com>
2022-07-04 08:57:15 -0700

    Tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+
 /**main - Entry point for this program
  *to find if generated number is a positive or negative or zero
  *Return: zero for success
  */
 int main(void)
 {
+
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
